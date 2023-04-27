0d010ca2354505ef25687806909bfb3904074ef6
Amanda Gray <floridabelle2021@protonmail.com>
2023-04-26 23:18:31 -0600

    added to my server.js

--- server.js
+++ server.js
@@ -1,3 +1,18 @@
 const express = require("express");
 const compression = require("compression");
-const apiRoutes = require("./routes")
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+
+//creating a port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// this is parsing, static, and route middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(compression());
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
